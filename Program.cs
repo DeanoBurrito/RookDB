75d05f88420ff905f39135418ee2e3af8ab08a0b
Dean Tuckey <dean243@hotmail.com>
2021-05-02 20:53:16 +1000

    V2 dev initial commit - clean slate

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CliMod;
 
 namespace RookDB
 {
@@ -7,33 +6,6 @@ namespace RookDB
     {
         static void Main(string[] args)
         {
-            Logger.Init(Logger.InitLevel.Verbose);
-            bool runEditor = false;
-            string editorFile = null;
-            for (int i = 0; i < args.Length; i++)
-            {
-                string arg = args[i];
-                if (arg == "-e" || arg == "--editor")
-                {
-                    runEditor = true;
-                }
-                if (arg == "-f" || arg == "--editor-file")
-                {
-                    if (i + 1 < args.Length)
-                    {
-                        editorFile = args[i + 1];
-                    }
-                }
-            }
-
-            if (runEditor)
-            {
-                Editor.EditorProgram.RunEditor(editorFile);
-            }
-            else
-            {
-                new CLInterface().Run();
-            }
-        }
+            Console.WriteLine("Hello world, from v2 development branch.");        }
     }
 }
